8859700f9bdf88d5c342f44027e2b5e3d7ee98cf
Kyle Maulsby <kylemaulsby@Kyles-MacBook-Air.local>
2019-06-19 12:30:52 -0500

    branch loops+exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,16 +1,24 @@
 (function(){
 "use strict";
 
-do {
-    var number = prompt("Pick an odd number between 1 and 50.");
-} while (number === null);
-console.log("Skipping number "+number+"!");
-for(var i = 1; i<50; i+=2){
-    if(i === Number(number)){
-        console.log("Skipping number "+ number+"!");
-        continue;
-    }
-    console.log("Here is the odd number "+i+"!");
+function isNumeric(value){
+    return Number(parseFloat(value)) === Number(value);
 }
+
+do {
+ var number = prompt("Please pick an odd number between 1 and 50.");
+    if((number % 2 === 1) && (number < 50)){
+        console.log("Skipping number "+number+"!");
+        for(var i = 1; i<50; i+=2){
+            if(i === Number(number)){
+                console.log("Skipping number "+ number+"!");
+                continue;
+            }
+            console.log("Here is the odd number "+i+"!");
+        }
+    }else {
+        number = false;
+    }
+} while(isNumeric(number) === false);
 
 })();
