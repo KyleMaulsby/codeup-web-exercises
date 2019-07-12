04db64ec7bdba3dff55ef483bfaa576056f744c1
Kyle Maulsby <kylemaulsby15@gmail.com>
2019-07-12 13:33:14 -0500

    geocoding mess

--- js/weather_map.js
+++ js/weather_map.js
@@ -2,15 +2,15 @@ var js;
 var wea;
 var fore;
 var climacon;
-var longlat = "29.4241,-98.4936";
-var area = "San Antonio, Texas";
+var longlat;
+var area;
+// var area = "San Antonio Texas";
 //function to get weather forcast into human readable text
 var forCast = function(data,jso,ind) {
     jso.forEach(function (ele) {
         if (data.hourly.data[ind*16].icon === ele.icon) {
             fore = ele.type;
             climacon = ele.image;
-            console.log(fore);
         }
     });
 };
@@ -19,8 +19,7 @@ var dayHtml = function(data,ind){
     var html = "";
     html += "<h3>"+data.daily.data[ind].temperatureLow+" / "+data.daily.data[ind].temperatureHigh+"</h3>";
     html += "<img src='"+climacon+"' alt='a'>";
-    html += "<p><b>"+fore+":</b> "+data.hourly.data[0].summary+"</p>";
-    console.log(fore);
+    html += "<p><b>"+fore+":</b> "+data.hourly.data[ind*16].summary+"</p>";
     html += "<p><b>Humidity:</b> "+(data.daily.data[ind].humidity*100).toFixed(0)+"%</p>";
     html += "<p><b>Wind Speed:</b> "+data.daily.data[ind].windSpeed+"</p>";
     html += "<p><b>Air Pressure:</b> "+data.daily.data[ind].pressure.toFixed(0)+"</p>";
@@ -35,32 +34,47 @@ var makeDay = function(data,ind) {
     });
 };
 //function to take info and build html for each set of days
-var updateWeather = function(geo) {
-    $.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSky + "/" + longlat.split(",")).done(function (data) {
+var updateWeather = function() {
+    geocode(area, token).then(function (geo) {
+        longlat = geo.reverse();
+        $.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSky + "/"+(longlat.join(','))).done(function (data) {
         wea = data;
+        $("#area").html(area);
         for (var i = 0; i <= 2; i++) {
             makeDay(data, i)
         }
-        var marker = new mapboxgl.Marker()
-            .setLngLat(geo)
-            .addTo(map);
+        var marker = new mapboxgl.Marker({
+            draggable: true
+        })
+        .setLngLat(longlat.reverse())
+        .addTo(map);
+        map.panTo(longlat, {
+            zoom: 10,
+            duration: 3000,
+            animate: true
+            });
+        });
     });
 };
 //geocode for the data entered
-// $("#refresh").on("click",function(){
-//     // area = $("#geo").val();
-//     geocode(area, "pk.eyJ1Ijoia3lsZW1hdWxzYnkiLCJhIjoiY2p4YXZpYnIyMDFqZzN1bGl3ejVqcmVodCJ9.PQ3odp_vmyva8RDga2Cw7g").then(function (geo) {
-//         longlat = geo.split(",");
-//         updateWeather(longlat)
-//     });
-// });
+$("#refresh").on("click",function(){
+    area = $("#geo").val();
+    updateWeather()
+});
 //calling the function to built the default html
-updateWeather(longlat);
+
+updateWeather();
 //building base map targeting SA
 mapboxgl.accessToken = token;
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
-    zoom: 10,
-    center: [-98.4916, 29.4252]
+    zoom: 1
+    // center: [-98.4916, 29.4252]
 });
+map.addControl(new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true
+}));
